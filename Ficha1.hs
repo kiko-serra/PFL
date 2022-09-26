d978bc0918ace34538bb584884224a2b3f6a0af2
kiko-serra <franciscopimentelserra@gmail.com>
2022-09-26 10:23:34 +0100

    Added some exercices

--- Ficha1.hs
+++ Ficha1.hs
@@ -41,14 +41,35 @@ initWithReverse x = reverse (tail (reverse x))
 binom :: Integer -> Integer -> Integer
 binom n k = let b = product [1..n]
                 a = n-k
-            in b `div` b * (product [1..a])
+            in b `div` b * product [1..a]
             
 --1.5 with where
 binom2 :: Integer -> Integer -> Integer
-binom2 n k = b `div` b * (product [1..a])
+binom2 n k = b `div` b * product [1..a]
                 where b = product [1..n]
                       a = n-k
 --1.6
 raizes :: Float -> Float -> Float -> (Float, Float)
 raizes a b c = ((-b + e) / (2 * a) , (-b - e) / (2 * a))
                         where e = sqrt b*b - 4 * a * c
+
+--1.7
+--[′a′, ′b′, ′c′] :: [Char]
+--(′a′, ′b′, ′c′) :: [(Char, Char, Char)]
+--[(False,′0′), (True,′1′)] :: [(Bool, Char)]
+--([False,True], [′0′,′1′]) :: ([Bool], [Char])
+--[tail, init, reverse] :: [([a]->[a])]
+--[id, not] :: [(Bool -> Bool)]
+
+--1.8
+segundo :: [a] -> a
+segundo xs = head (tail xs)
+
+trocar :: (b, a) -> (a, b)
+trocar (x, y) = (y, x)
+
+par :: a -> b -> (a, b)
+par x y = (x, y)
+
+dobro :: Num a => a -> a
+dobro x = 2 * x
