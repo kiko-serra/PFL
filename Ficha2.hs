670294c9e994651ed44ebb2f127596c89b6bd234
kiko-serra <franciscopimentelserra@gmail.com>
2022-09-29 18:26:09 +0100

    Done more exercices

--- Ficha2.hs
+++ Ficha2.hs
@@ -1,3 +1,4 @@
+import Data.Time (CalendarDiffDays(cdDays))
 --2.1 a)
 myand :: [Bool] -> Bool
 myand [] = True
@@ -56,3 +57,22 @@ isort [x] = [x]
 isort (x:xs) = insert x (isort xs)
 
 --2.5 a)
+myminimum :: Ord a => [a] -> a
+minumum [] = []
+myminimum [a] = a
+myminimum (x : y : xs)
+  | x < y = myminimum (x : xs)
+  | otherwise = myminimum (y : xs)
+
+--2.5 b)
+mydelete :: Eq a => a -> [a] -> [a]
+mydelete a [] = []
+mydelete a (x:xs)
+  | x == a = xs
+  | otherwise = x : mydelete a xs
+
+--2.5 c)
+myssort :: Ord a => [a] -> [a]
+myssort [] = []
+myssort [x] = [x]
+myssort xs = minimum xs : myssort (mydelete (minimum xs) xs)
