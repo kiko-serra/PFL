f7e1ddb23a58b958d2b68d80a1b2c4b061b23d96
kiko-serra <franciscopimentelserra@gmail.com>
2022-10-06 10:27:54 +0100

    Done all ficha 2

--- Ficha2.hs
+++ Ficha2.hs
@@ -1,3 +1,5 @@
+import Data.Char
+import Data.List
 --2.1 a)
 myand :: [Bool] -> Bool
 myand [] = True
@@ -43,17 +45,17 @@ mdc a b
     | otherwise = mdc b (a `mod` b)
 
 --2.4 a)
-insert :: Ord a => a -> [a] -> [a]
-insert a [] = [a]
-insert a (x:xs)
+myInsert :: Ord a => a -> [a] -> [a]
+myInsert a [] = [a]
+myInsert a (x:xs)
     | a < x = a : x : xs
-    | otherwise = x : insert a xs
+    | otherwise = x : myInsert a xs
 
 --2.4 b)
 isort :: Ord a => [a] -> [a]
 isort [] = []
 isort [x] = [x]
-isort (x:xs) = insert x (isort xs)
+isort (x:xs) = myInsert x (isort xs)
 
 --2.5 a)
 myminimum :: Ord a => [a] -> a
@@ -108,3 +110,100 @@ pitagoricos n = [(x,y,z) | x <- [1..n],y <-[1..n] , z <-[1..n],x^2 + y^2 == z^2]
 --2.12
 primo :: Integer -> Bool
 primo n = length (divprop n) == 1
+
+--2.13
+isMerssene :: Integer -> Bool
+isMerssene n = primo n && not (null [i | i <-[0..n],(2^i - 1) == n])
+
+mersennes :: [Integer]
+mersennes = [i | i <- [1..], isMerssene i]
+
+--2.14
+binom :: Integer -> Integer -> Integer
+binom n k =  fact n `div` ( fact k * fact (n -k))
+    where fact n = product [1..n]
+
+pascal :: Integer -> [[Integer]]
+pascal n = [[binom i j | j <- [0..i]] | i <- [0..n]]
+
+--2.15
+cifraLetra :: Int -> Char -> Char 
+cifraLetra k x  
+    | isLetter x && isUpper x = chr (mod (ord x + k - ord 'A' ) 26 + ord 'A' )
+    | isLetter x && isLower x = chr (mod (ord x + k - ord 'a' ) 26 + ord 'a' )
+    | otherwise  = x
+
+cifrar :: Int -> String -> String
+cifrar k xs = [cifraLetra k x | x <- xs]
+
+--2.16
+compConcat :: [[a]] -> [a]
+compConcat xss =  [ x | xs <- xss,x <- xs]
+
+compReplicate :: Int -> a -> [a]
+compReplicate n x = [x | _ <-[1..n]]
+
+comBangBang :: Int -> [a] -> a
+comBangBang n xs = head [x | (x,y) <- zip xs [0..n],y==n]
+
+--2.17
+forte :: String -> Bool
+forte xs = (length xs >= 8) && hasUpper && hasLower && hasNumber
+  where 
+    hasUpper = not (null [x | x <- xs, isUpper x])
+    hasLower = not (null [x | x <- xs, isLower x])
+    hasNumber = not (null [x | x <- xs, isNumber x])
+
+--2.18 a)
+mindiv :: Int -> Int
+mindiv n  
+    | null z  = n
+    | otherwise  = head z 
+    where z = [ i | i <- [2..floor (sqrt (fromIntegral n))],mod n i == 0]
+
+--2.18 b)
+primo2:: Int -> Bool
+primo2 n = n >1 &&  (mindiv n  == n)
+
+--2.19
+recNub :: Eq a => [a] -> [a] 
+recNub []  = []
+recNub (x:xs) = x:recNub [y | y<-xs ,y /=x]
+
+--2.20
+transpose :: [[a]] -> [[a]]
+transpose xss  = [ [xs !! i | xs <- xss] | i<- [0..(z-1)]  ]
+    where z = length (head xss)
+
+--2.21
+algarismos :: Int -> [Int]
+algarismos n 
+    | n < 10  =  [n]
+    | otherwise =  algarismos (div n 10) ++ [mod n 10]
+
+--2.22
+toBits :: Int -> [Int]
+toBits 0 = [0]
+toBits 1 = [1]
+toBits n = toBits(div n 2) ++ [mod n 2] 
+
+--2.23
+fromBits :: [Int] -> Int
+fromBits xs = sum [x*y | (x,y)<- zip xs (iterate (`div` 2) (2^(length xs - 1)))]
+
+--2.24 a)
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys)
+  | x < y = x : merge xs (y:ys)
+  | otherwise = y: merge (x:xs) ys
+
+--2.24 b)
+
+
+
+--permutacoes
+permutacoes :: [Int] -> [[Int]]
+permutacoes [] = [[]]
+permutacoes xs = [x:l | x<- xs, l<- permutacoes(xs\\[x])]
