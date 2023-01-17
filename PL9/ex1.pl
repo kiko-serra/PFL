80a0512eb3263d0521855a13811b5f1994790cc0
kiko-serra <franciscopimentelserra@gmail.com>
2023-01-17 22:17:06 +0000

    Done more exs

--- PL9/ex1.pl
+++ PL9/ex1.pl
@@ -1,8 +1,27 @@
 double(X, Y):- Y is X*2.
 
-map(Pred, [], []).
-map(Pred, [H|T], L2):- length(L1, Index),
-                    Index >0,
-                    I1 is Index-1,
-                    map(Pred, T, L3),
-                    L2 = []
+map(P, [H], [R]) :-  G =.. [P, H, R], G.
+map(Pred, [H|T], [R|RT]):- 
+                            G=.. [Pred, H, R],
+                            G,
+                            map(Pred, T, RT). 
+
+
+sum(A, B, C):- C is A+B.
+fold(Pred, StartValue, [H], Final):- G=.. [Pred, H, StartValue, Final], G.
+fold(Pred, StartValue, [H|T], Final):- 
+                                        G=.. [Pred, StartValue, H, Final1],
+                                        G,
+                                        fold(Pred, Final1, T, Final).
+
+
+separate([], _, [], []).
+separate([H|T], P, [H|Yes], No) :- 
+                G =.. [P, H],
+                G,
+                separate(T, P, Yes, No).
+
+separate([H|T], P, Yes, [H|No]) :- 
+                G =.. [P, H],
+                \+ G,
+                separate(T, P, Yes, No).
